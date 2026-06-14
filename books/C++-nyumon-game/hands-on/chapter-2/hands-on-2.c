ce78b8669eb3243b8b4be0dcd7308da7f6a8c4c2
jellyfish <ayame3776@gmail.com>
2026-06-14 19:45:41 +0900

    Create hands-on-2.c

---
+++ books/C++-nyumon-game/hands-on/chapter-2/hands-on-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // <stdio.h>をインクルード
+
+int main(void) // main関数の定義
+{
+	int score = 1000000; //整数を扱う変数に値を代入
+	printf("スコアは%d", score); // printf()で変数の値を出力
+}
+
+/*
+%d は、出力するデータの表示形式を指定する変換指定子。
+*/
