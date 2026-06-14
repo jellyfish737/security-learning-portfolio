6cb80abb6e058b04943532d31609a9a11a51670a
jellyfish <ayame3776@gmail.com>
2026-06-14 20:32:55 +0900

    Create variable_1.c

---
+++ books/C++-nyumon-game/hands-on/chapter-2/variable_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // <stdio.h>をインクルード
+
+int main(void) // main関数の定義
+{
+	int score = 777; // 整数を扱う変数に初期値を代入
+	float pi = 2.17; // 小数を扱う変数に初期値を代入
+	char c = 'A'; // 文字を扱う変数に初期値を代入
+	printf("scoreの値は%d\n", score); // printf()で整数を出力
+	printf("piの値は%f\n", pi); // printf()で小数を出力
+	printf("cの値は%c\n", c); // printf()で文字を出力
+}
